657e4edc9e82f8890cbab9c029e28bf4e51e519d
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2021-05-07 18:57:58 +0200

    Update dependencies and version number

--- app/src/main/java/com/akiel/shakeitoff/ShakerService.java
+++ app/src/main/java/com/akiel/shakeitoff/ShakerService.java
@@ -12,9 +12,9 @@ import android.os.Build;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.os.Vibrator;
-import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.NotificationCompat;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
